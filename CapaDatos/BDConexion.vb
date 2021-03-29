c6671f0f11ed15dae7d36fd737c730c2140306b4
jhony soto <jhalsolo@gmail.com>
2021-03-29 19:06:19 +0100

    Add project files.

---
+++ CapaDatos/BDConexion.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Public Class BDConexion
+    Dim ConexionSQL As SqlConnection 'Atributo que contendra la conexion sql
+
+
+    Public Function Conectar() As SqlConnection
+        ConexionSQL = New SqlConnection("Data source=DESKTOP-7GVHR1M\SQLEXPRESS;Initial Catalog=Banco_Sena;integrated security=true")
+        Return ConexionSQL
+
+    End Function
+
+
+
+
+
+End Class

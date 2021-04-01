2a122fd6e4f5b7ad2adc6fb541c2623a59c6762c
oeruizzz <oeruizh@gmail.com>
2021-04-01 12:52:14 -0500

    minusculas

--- CapaDatos/BDConexion.vb
+++ CapaDatos/BDConexion.vb
@@ -1,16 +1,12 @@
 ﻿Imports System.Data.SqlClient
 Imports System.Data
 Public Class BDConexion
-    Dim ConexionSQL As SqlConnection 'Atributo que contendra la conexion sql
+    Dim conexionSQL As SqlConnection 'Atributo que contendra la conexion sql
 
 
-    Public Function Conectar() As SqlConnection
-        ConexionSQL = New SqlConnection("Data source=DESKTOP-7GVHR1M\SQLEXPRESS;Initial Catalog=Banco_Sena;integrated security=true")
-        Return ConexionSQL
+    Public Function conectar() As SqlConnection
+        conexionSQL = New SqlConnection("Data source=DESKTOP-7GVHR1M\SQLEXPRESS;Initial Catalog=Banco_Sena;integrated security=true")
+        Return conexionSQL
     End Function
-
-
-
-
 
 End Class
